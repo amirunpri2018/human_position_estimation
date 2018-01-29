554625e5a93fd2888090d79470d14b1761a51636
Ilyass Taouil <sc14it@leeds.ac.uk>
2018-01-29 16:53:51 +0000

    Modified person detection module to be a ROS service to avoid cv_bridge issues in OpenCV3.

--- modules/human_detection.py
+++ modules/human_detection.py
@@ -1,83 +1,96 @@
 #!/usr/bin/env python
 
 """
-    Detection.
+    Human Detection Service.
 
-    The class is able to detect the
-    presence of humans in the image
-    feed of the robot.
+    ROS service that takes as input
+    an image in a MAT format, and
+    returns a frame with detections.
 """
 
 # import the necessary packages
-from cv_bridge import CvBridge, CvBridgeError
-from imutils.video import VideoStream
-from imutils.video import FPS
-import numpy as np
-import argparse
-import imutils
-import time
 import cv2
-import os
+import imutils
+import numpy as np
 
-class HumanDetection:
+def handle_person_detection(self, frame):
+    """
+        Returns the frame with
+        detections bounding boxes.
 
-    def __init__(self):
-        """ Class constructor """
+        Params:
+            MAT: Image with MAT format
 
-        self.bridge = CvBridge()
+        Ouput:
+            MAT: Image with detections boxes
+    """
 
-        # Define detection's target/s
-        self.target = ["person"]
+    print("Request: ", frame)
 
-        # Bounding boxes colours
-        self.colours = np.random.uniform(0, 255, size=(len(self.target), 3))
+    # Define detection's target/s
+    targets = ["person"]
 
-        # Load NN's serialised model
-        print("[INFO] loading model...")
-        self.net = cv2.dnn.readNetFromCaffe("/home/itaouil/tiago_ws/src/human_aware_robot_navigation/src/HARN/modules/MobileNetSSD_deploy.prototxt.txt",
-                                            "/home/itaouil/tiago_ws/src/human_aware_robot_navigation/src/HARN/modules/MobileNetSSD_deploy.caffemodel")
+    # Bounding boxes colours
+    colours = np.random.uniform(0, 255, size=(len(targets), 3))
 
-    def detect(self, frame):
-        """
-            Returns the frame with recognitions.
-        """
+    # Load NN's serialised model
+    net = cv2.dnn.readNetFromCaffe("/home/itaouil/tiago_ws/src/human_aware_robot_navigation/src/HARN/modules/MobileNetSSD_deploy.prototxt.txt",
+                                   "/home/itaouil/tiago_ws/src/human_aware_robot_navigation/src/HARN/modules/MobileNetSSD_deploy.caffemodel")
 
-        # Save frame in memory
-        print("PRINT: ", os.path.abspath(os.path.join(os.path.dirname( __file__ ))))
-        cv2.imwrite(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'frame.png')), frame)
+    # Resize image to be maximum 400px wide
+    frame = imutils.resize(frame, width = 400)
 
-        # Resize image to be maximum 400px wide
-        frame = imutils.resize(frame, width = 400)
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
 
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    print("[INFO] Running detection...")
+    net.setInput(blob)
+    detections = net.forward()
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        net.setInput(blob)
-        detections = net.forward()
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
 
-        # loop over the detections
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the prediction
-            confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > 0.8:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
 
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence > 0.8:
-                # extract the index of the class label from the
-                # `detections`, then compute the (x, y)-coordinates of
-                # the bounding box for the object
-                idx = int(detections[0, 0, i, 1])
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
+            # draw the prediction on the frame
+            label = "{}: {:.2f}%".format(targets[idx], confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), colours[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colours[idx], 2)
 
-                # draw the prediction on the frame
-                label = "{}: {:.2f}%".format(self.target[idx], confidence * 100)
-                cv2.rectangle(frame, (startX, startY), (endX, endY), self.colours[idx], 2)
-                y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.colours[idx], 2)
+    return RunDetectionResponse(frame)
 
-        return frame
+def main(args):
+
+    try:
+        # Setting up service
+        print("[INFO] Detection service set up...")
+
+        # Start node
+        rospy.init_node('person_detection')
+
+        # Create service
+        s = rospy.Service('person_detection', RunDetection, handle_person_detection)
+
+        # Log acknowledgement
+        print("[INFO] Ready to detect humans...")
+
+        # Spin it, baby !
+        rospy.spin()
+
+    except KeyboardInterrupt as e:
+        print('Error during main execution' + e)
