3f79182d17d21d90f29ef9458129ef79bd401754
Ilyass Taouil <itaouil95@gmail.com>
2018-04-24 11:38:54 +0100

    Got rid of redundant folders.

--- waypoints/graphs/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# Export files
-# from logik import logik_graph
-# from long_room import long_room_graph
-from simulator import simulator_graph
--- waypoints/helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-# Export files
-from gotopose import GoToPose
